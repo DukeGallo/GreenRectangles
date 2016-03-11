7c96ee29b30960deaa2ec87517d2a8fdc5fae365
Duke Gallo <dukegallo@gmail.com>
2016-03-11 12:13:30 -0800

    Server up and running.

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var sportsAPI = require('./api.js');
+
+app.use('/',express.static('./public'));
+app.use('/query', foodApi);
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/form.js', function(req, res) {
+  res.sendFile(__dirname + './form.js');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port);
